934f905fbbf3db1374d00549404fb8b2b6679ae4
Seyoung Yoon <stacey.exp@gmail.com>
2021-03-24 16:16:23 -0700

    server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ if (process.env.NODE_ENV === 'production') {
 // Define API routes here
 // Send every other request to the React app
 // Define any API routes before this runs
+app.use(express.static('./client/build/'));
+app.use('/', express.static('./client/build/index.html'));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/public/index.html'));
 });
